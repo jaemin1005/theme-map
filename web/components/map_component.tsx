0dc28043a7737936ab08ea4d2aaa3cff0e51775c
jaemin1005 <ghkdwoals1005@gmail.com>
2024-09-07 23:32:10 +0900

    Fix | 기본적으로 제공하는 zoomControl 버튼 제거

--- web/components/map_component.tsx
+++ web/components/map_component.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef } from "react";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 
@@ -15,13 +15,14 @@ interface MapComponentProps {
 // 맵 컴포넌트
 const MapComponent: React.FC<MapComponentProps> = ({ center, zoom }) => {
   const mapRef = useRef<L.Map | null>(null);
-  const [currentCenter, setCurrentCenter] = useState<[number, number]>(center);
+  // const [currentCenter, setCurrentCenter] = useState<[number, number]>(center);
 
   useEffect(() => {
     // 지도 초기화
     mapRef.current = L.map("map", {
       center: center,
       zoom: zoom,
+      zoomControl: false,
       // 드래그 기능 On
       dragging: true,
       // 관성 스크롤 활성화하여 사용자 경험을 극대화한다.
@@ -34,17 +35,6 @@ const MapComponent: React.FC<MapComponentProps> = ({ center, zoom }) => {
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(mapRef.current);
 
-    // 드래그 이벤트 리스너 추가
-    // 드래그 시 맵을 이동한다.
-    mapRef.current.on("drag", () => {
-      if (mapRef.current) {
-        // 현재 중심 좌표를 가져와 설정한다.
-        const newCenter = mapRef.current.getCenter();
-        setCurrentCenter([newCenter.lat, newCenter.lng]);
-      }
-    });
-
-    // 컴포넌트가 언마운트될 때 지도 제거
     return () => {
       if (mapRef.current) {
         mapRef.current.remove();
@@ -54,11 +44,7 @@ const MapComponent: React.FC<MapComponentProps> = ({ center, zoom }) => {
 
   return (
     <>
-      <div id="map" className="w-full h-full"/>
-      <div>
-        현재 중심 좌표: {currentCenter[0].toFixed(4)},{" "}
-        {currentCenter[1].toFixed(4)}
-      </div>
+      <div id="map" className="w-full h-full z-0"/>
     </>
   );
 };
