ff6787d2906d74296bf18498c0dca35f8b1d966c
jaemin1005 <ghkdwoals1005@gmail.com>
2024-09-08 14:23:15 +0900

    Feat | Dom이 랜더링 된 후에 맵을 초기화하도록 하였다

--- web/components/map_component.tsx
+++ web/components/map_component.tsx
@@ -4,49 +4,49 @@ import React, { useEffect, useRef } from "react";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 
-// MapComponentProps
 interface MapComponentProps {
-  // 중심좌표
   center: [number, number];
-  // 줌레벨
   zoom: number;
 }
 
-// 맵 컴포넌트
 const MapComponent: React.FC<MapComponentProps> = ({ center, zoom }) => {
   const mapRef = useRef<L.Map | null>(null);
-  // const [currentCenter, setCurrentCenter] = useState<[number, number]>(center);
+  const mapContainerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    // 지도 초기화
-    mapRef.current = L.map("map", {
-      center: center,
-      zoom: zoom,
-      zoomControl: false,
-      // 드래그 기능 On
-      dragging: true,
-      // 관성 스크롤 활성화하여 사용자 경험을 극대화한다.
-      inertia: true,
-    });
+    // DOM이 랜더링 된 후에만 변경하도록 하였다.
+    if (!mapContainerRef.current) return;
 
-    // OpenStreetMap 타일 레이어 추가
-    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-      attribution:
-        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    }).addTo(mapRef.current);
+    // 맵 초기화
+    if (!mapRef.current) {
+      mapRef.current = L.map(mapContainerRef.current, {
+        center: center,
+        zoom: zoom,
+        zoomControl: false,
+        dragging: true,
+        inertia: true,
+      });
 
+      // 타일 레이어 즉시 추가
+      L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(mapRef.current);
+    } else {
+      // 맵의 중심을 업데이트
+      mapRef.current.setView(center, zoom);
+    }
+
+    // 컴포넌트 언마운트 시 맵 정리
     return () => {
       if (mapRef.current) {
         mapRef.current.remove();
+        mapRef.current = null;
       }
     };
   }, [center, zoom]);
 
-  return (
-    <>
-      <div id="map" className="w-full h-full z-0"/>
-    </>
-  );
+  return <div ref={mapContainerRef} className="w-full h-full z-0" />;
 };
 
 export default MapComponent;
