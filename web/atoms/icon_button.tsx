73b5eef0a8bd63c26d9fb3d7fa6f1fbeff4a57c8
jaemin1005 <ghkdwoals1005@gmail.com>
2024-09-09 14:43:58 +0900

    Feat | 클릭 이벤트 추가

--- web/atoms/icon_button.tsx
+++ web/atoms/icon_button.tsx
@@ -3,10 +3,13 @@ import React from "react";
 // children에는 icon이 들어간다.
 const IconButton: React.FC<{
   children: React.ReactNode;
+  onClick?: () => void;
   className?: string;
-}> = ({ children, className }) => {
+}> = ({ children, className, onClick }) => {
   return (
-    <div  className={`group bg-gray-800 bg-opacity-60 hover:bg-opacity-100 rounded-full p-2 ${className}`}>
+    <div
+      className={`group bg-gray-800 bg-opacity-60 hover:bg-opacity-100 rounded-full p-2 ${className}`} onClick={onClick}
+    >
       {children}
     </div>
   );
