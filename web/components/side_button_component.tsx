a5e7b9ef6dfdbe9b568656701f65e2348a329d44
jaemin1005 <ghkdwoals1005@gmail.com>
2024-09-09 14:44:25 +0900

    Feat | 버튼 이벤트에 대한 Props를 생성, Props를 통해 이벤트를 할당한다

--- web/components/side_button_component.tsx
+++ web/components/side_button_component.tsx
@@ -1,27 +1,37 @@
 import AddIcon from "@mui/icons-material/Add";
 import IconButton from "@/atoms/icon_button";
-import RemoveIcon from '@mui/icons-material/Remove';
-import ControlCameraIcon from '@mui/icons-material/ControlCamera';
-import CommentIcon from '@mui/icons-material/Comment';
-import FavoriteIcon from '@mui/icons-material/Favorite';
+import RemoveIcon from "@mui/icons-material/Remove";
+import ControlCameraIcon from "@mui/icons-material/ControlCamera";
+import CommentIcon from "@mui/icons-material/Comment";
+import FavoriteIcon from "@mui/icons-material/Favorite";
 
-const SideButtonComponent: React.FC = () => {
+interface SideButtonComponentProps {
+  zoomInFunc: () => void;
+  zoomOutFunc: () => void;
+  moveToCurrent: () => void;
+}
+
+const SideButtonComponent: React.FC<SideButtonComponentProps> = ({
+  zoomInFunc,
+  zoomOutFunc,
+  moveToCurrent,
+}) => {
   return (
-    <div className="absolute h-full flex flex-col z-50 right-0 pt-5 items-center px-2 gap-y-1">
-      <IconButton>
+    <div className="absolute h-full flex flex-col right-0 pt-5 items-center px-2 gap-y-1 top-0 z-50">
+      <IconButton onClick={zoomInFunc}>
         <AddIcon className="text-white text-2xl" />
       </IconButton>
-      <IconButton>
-        <RemoveIcon className="text-white text-2xl"/>
+      <IconButton onClick={zoomOutFunc}>
+        <RemoveIcon className="text-white text-2xl" />
       </IconButton>
-      <IconButton className="mt-5">
-        <ControlCameraIcon className="text-white text-2xl group-hover:text-yellow-300"/>
-      </IconButton>
-      <IconButton>
-        <CommentIcon className="text-white text-2xl group-hover:text-green-300"/>
+      <IconButton className="mt-5" onClick={moveToCurrent}>
+        <ControlCameraIcon className="text-white text-2xl group-hover:text-yellow-300" />
       </IconButton>
       <IconButton>
-        <FavoriteIcon className="text-white text-2xl group-hover:text-pink-300"/>
+        <CommentIcon className="text-white text-2xl group-hover:text-green-300" />
+      </IconButton>
+      <IconButton>
+        <FavoriteIcon className="text-white text-2xl group-hover:text-pink-300" />
       </IconButton>
     </div>
   );
