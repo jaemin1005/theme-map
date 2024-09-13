ebb984281c8035e635449e10f4681c2979d3ada0
jaemin1005 <ghkdwoals1005@gmail.com>
2024-09-13 15:47:21 +0900

    Feat | 모달창에 이미지를 업로드 기능 및 이미지슬라이더를 추가하였다

--- web/molecules/write_modal.tsx
+++ web/molecules/write_modal.tsx
@@ -1,49 +1,98 @@
-import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
-import Modal from "@mui/material/Modal";
+"use client";
 
-const style = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: 600,
-  bgcolor: "background.paper",
-  border: "2px solid #000",
-  boxShadow: 24,
-  p: 4,
-
-  "@media (max-width: 600px)": {
-    width: "100%",
-    p: 2,
-  },
-};
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+} from "@nextui-org/modal";
+import { Button } from "@nextui-org/button";
+import { Button as MuiButton } from "@mui/material";
+import { useRef, useState } from "react";
+import TextField from "@mui/material/TextField";
+import AddAPhotoIcon from "@mui/icons-material/AddAPhoto";
+import { ImageSlider } from "@/atoms/image_slider";
 
 interface WriteModalProps {
   open: boolean;
-  handleCloseModalEv: () => void;
+  onOpenChange: () => void;
 }
 
 export const WriteModal: React.FC<WriteModalProps> = ({
   open,
-  handleCloseModalEv: closeEv,
+  onOpenChange,
 }) => {
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
+  const [files, setFiles] = useState<File[]>([]);
+
+  // 도달창을 닫을 때, 리액트훅 초기화
+  const handlerClose = () => {
+    setFiles([])
+  }
+
+  // 버튼 클릭했을 때, input DOM을 클릭하게 한다.
+  const handleButtonClick = () => {
+    fileInputRef.current?.click();
+  };
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const files = event.target.files;
+    if (files && files.length > 0) {
+      setFiles((prev) => [...prev, ...Array.from(files)]);
+    }
+  };
+
   return (
     <div>
       <Modal
-        open={open}
-        onClose={closeEv}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
+        backdrop="opaque"
+        isOpen={open}
+        placement="center"
+        onOpenChange={onOpenChange}
+        onClose={handlerClose}
+        classNames={{
+          backdrop:
+            "bg-gradient-to-t from-zinc-900 to-zinc-900/10 backdrop-opacity-20",
+        }}
+        scrollBehavior={"outside"}
       >
-        <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
-        </Box>
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">
+                <MuiButton
+                  variant="contained"
+                  startIcon={<AddAPhotoIcon />}
+                  className="w-fit"
+                  onClick={handleButtonClick}
+                >
+                  Add Photo
+                </MuiButton>
+              </ModalHeader>
+              <ModalBody>
+                <ImageSlider files={files}></ImageSlider>
+                <TextField label="Title" variant="outlined" />
+                <TextField label="Body" multiline rows={10} />
+              </ModalBody>
+              <ModalFooter>
+                <Button color="danger" variant="light" onPress={onClose}>
+                  Close
+                </Button>
+                <Button color="primary" onPress={onClose}>
+                  Action
+                </Button>
+              </ModalFooter>
+              <input
+                type="file"
+                ref={fileInputRef}
+                onChange={handleFileChange}
+                accept="image/*"
+                style={{ display: "none" }}
+              />
+            </>
+          )}
+        </ModalContent>
       </Modal>
     </div>
   );
