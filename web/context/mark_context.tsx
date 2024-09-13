828a29a50370193fca9f5236ab851fc0ddbd0b1f
jaemin1005 <ghkdwoals1005@gmail.com>
2024-09-13 20:48:01 +0900

    New | 여러 마크와 그에 대한 상세 정보를 관리하는 컨텍스트를 생성

---
+++ web/context/mark_context.tsx
@@ -0,0 +1,44 @@
+'use client'
+
+import { createContext, useState, ReactNode, useContext } from "react";
+
+// 각 마크의 상세정보를 담기위한 인터페이스
+export interface Mark {
+  files: File[];
+  title: string;
+  body: string;
+  point: [number, number];
+}
+
+// context의 타입
+interface MarkInfo {
+  marks: Mark[];
+  addMark: (mark: Mark) => void;
+}
+
+const MarkContext = createContext<MarkInfo | null>(null);
+
+export const MarkProvider = ({ children }: { children: ReactNode }) => {
+  const [marks, setMarks] = useState<Mark[]>([]);
+
+  const addMark = (mark: Mark) => {
+    setMarks((prev) => [...prev, mark]);
+  };
+
+  return (
+    <MarkContext.Provider value={{ marks, addMark }}>
+      {children}
+    </MarkContext.Provider>
+  );
+};
+
+// constext에 대한 커스텀 훅 정의 
+export const useMark = () => {
+  try {
+    const context = useContext(MarkContext);
+    return context;
+  } catch (err) {
+    //! Provider의 외에서 사용할 경우 에러
+    console.error("useMarkInfo must be used within an MarkProvider")
+  }
+};
