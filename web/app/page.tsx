f57b79b8de8321d9f451a229dad6d442217c8d5b
jaemin1005 <ghkdwoals1005@gmail.com>
2024-09-14 20:52:39 +0900

    Feat | 메인 페이지에 모달창, MarkerComponent를 구현하였다
    맵 클릭에 대한 함수 및, 모달창 Save 버튼에 대한 콜백함수 구현

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -6,9 +6,15 @@ import dynamic from "next/dynamic";
 import { useEffect, useState, useRef } from "react";
 import L from "leaflet";
 import SideButtonComponent from "@/components/side_button_component";
+import { WriteModal } from "@/molecules/write_modal";
+import { useDisclosure } from "@nextui-org/modal";
+import { useMark } from "@/context/mark_context";
 
 // 클라이언트에서만 랜더링 되도록 설정한다.
 const MapComponent = dynamic(() => import("../components/map_component"), {
+  ssr: false,
+});
+const MarkerComponent = dynamic(() => import("../components/marker_component"), {
   ssr: false,
 });
 
@@ -16,8 +22,15 @@ export default function Home() {
   const [currentLocation, setCurrentLocation] = useState<[number, number]>(
     INIT_LOCATION_INFO.COORDINATE
   );
-
   const mapRef = useRef<L.Map | null>(null);
+  const [clickPosition, setClickPosition] = useState<[number, number] | null>(
+    null
+  );
+
+  const [mark, setMark] = useState<boolean>(false);
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
+  const { marks, addMark } = useMark();
 
   //const [isLoading, setIsLoading] = useState(true);
 
@@ -30,27 +43,61 @@ export default function Home() {
             position.coords.latitude,
             position.coords.longitude,
           ]);
-          //setIsLoading(false);
         },
         (error) => {
           console.error(ERROR_MSG.GEOLOCATION_GETTING_FAIL, error);
-          //setIsLoading(false);
         }
       );
     } else {
       console.log(ERROR_MSG.GEOLOCATION_NOT_AVAILABLE);
-      //setIsLoading(false);
     }
   }, []);
+
+  const onMapClick = (event: L.LeafletMouseEvent) => {
+    if (mark) {
+      setClickPosition([event.latlng.lat, event.latlng.lng]);
+      onOpen();
+    }
+  };
+
+  const cbSaveBtn = (files: File[], title: string, body: string) => {
+
+    if(clickPosition === null) return;
+
+    addMark({
+      files,
+      title,
+      body,
+      point: clickPosition,
+    });
+  };
 
   return (
     <div className="w-screen h-screen relative">
       <MapComponent
         center={currentLocation}
         zoom={INIT_LOCATION_INFO.ZOOM}
+        onMapReady={(map) => {
+          mapRef.current = map;
+        }}
+        onMapClick={onMapClick}
+      >
+        <MarkerComponent></MarkerComponent>
+      </MapComponent>
+      <SideButtonComponent
         mapRef={mapRef}
-      ></MapComponent>
-      <SideButtonComponent mapRef={mapRef} />
+        toggleMark={[
+          mark,
+          () => {
+            setMark((prev) => !prev);
+          },
+        ]}
+      />
+      <WriteModal
+        open={isOpen}
+        onOpenChange={onOpenChange}
+        cbSaveBtn={cbSaveBtn}
+      />
     </div>
   );
 }
