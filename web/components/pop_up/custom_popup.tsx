8a50b215ddd82858d91c4249037f0173e43f83d4
jaemin1005 <ghkdwoals1005@gmail.com>
2024-09-16 19:07:55 +0900

    Feat | CustomPopUp 기능 구현 완료
    file은 uri로 변경하여 이미지를 가져옵니다.
    title, body는 해당 마커를 설명할 문자열

---
+++ web/components/pop_up/custom_popup.tsx
@@ -0,0 +1,45 @@
+import Image from "next/image";
+import { Popup } from "react-leaflet";
+import "./popup.css";
+import { useEffect } from "react";
+
+interface CustomPopupProps {
+  file: File;
+  title: string;
+  body: string;
+}
+
+export const CustomPopup: React.FC<CustomPopupProps> = ({
+  file,
+  title,
+  body,
+}) => {
+  // 파일 Url로 변환
+  const fileToUrl = (file: File) => {
+    const url = URL.createObjectURL(file);
+    return url;
+  };
+
+  return (
+    <Popup className="custom-popup">
+      <div className="h-full flex flex-row min-w-[300px] max-w-[600px] gap-x-2">
+        {file && (
+          <Image
+            width={100}
+            height={100}
+            src={fileToUrl(file)}
+            alt="이미지"
+            objectFit="fit"
+            className="rounded-md"
+          ></Image>
+        )}
+        <div className="w-full overflow-hidden">
+          <h2 className="font-extrabold text-medium whitespace-nowrap">
+            {title}
+          </h2>
+          <p className="mt-2 text-wrap">{body}</p>
+        </div>
+      </div>
+    </Popup>
+  );
+};
