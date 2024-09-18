c8839121d08de163dee073a9d6defce8d45639d6
jaemin1005 <ghkdwoals1005@gmail.com>
2024-09-18 20:59:25 +0900

    Feat | 로그인 함수 및 토스트 컴포넌트 추가
    로그인함수는 모달창을 통해 사용자로부터 받은 이메일과 비밀번호를 사용하여 서버에 로그인 요청을 보내는 역할
    토스트 컴포넌트를 통해, 로그인의 성공 및 실패를 알 수 있게 사용자에게 피드백을 제공한다

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -12,6 +12,11 @@ import { SpeedDial } from "@/components/speed_dial_component";
 import PersonIcon from "@mui/icons-material/Person";
 import { LoginModal } from "@/components/modal/login_modal";
 import { LOGIN_MSG } from "@/static/login_msg";
+import { useAuth } from "@/context/auth_context";
+import { useToast } from "@/components/toast/toast_hook";
+import { ToastComponent } from "@/components/toast/toast_component";
+import { isValidEmail } from "@/Func/validate";
+import { TOAST_MSG } from "@/static/toast_msg";
 
 // 클라이언트에서만 랜더링 되도록 설정한다.
 const MapComponent = dynamic(() => import("../components/map_component"), {
@@ -35,13 +40,22 @@ export default function Home() {
 
   const [mark, setMark] = useState<boolean>(false);
 
+  const {open, type, msg, time, setOpen, showToast } = useToast();
+
   //#region  --modal 상태관리--
 
   const [isWriteModalOpen, setIsWriteModalOpen] = useState(false);
   const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
 
   //#endregion
+
+  //#region  --context 관리--
+
   const { marks, addMark } = useMark();
+
+  const { user, setUser, loading, logout, accessToken, setAccessToken } = useAuth();
+
+  //#endregion
 
   useEffect(() => {
     // 현 위치 좌표가져오기
@@ -80,6 +94,44 @@ export default function Home() {
     });
   };
 
+  /**
+   * 로그인 함수
+   * email과 password를 매개변수로 받아, 로그인을 한다.
+   * @param email userid
+   * @param password password
+   */
+  const loginFunc = async (email: string, password: string) => {
+
+    if(isValidEmail(email) === false) {
+      showToast(TOAST_MSG.INVALID_EMAIL, "error");
+      return;
+    }
+
+    try {
+      const response = await fetch("/api/auth/login", {
+        method: "POST",
+        credentials: "include", // 쿠키 포함
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        // 유저 저장
+        setUser(data.user);
+        // access 토큰 저장 
+        setAccessToken(data.accessToken); 
+      } else {
+        showToast(TOAST_MSG.LOGIN_FAIL, "error");
+      }
+    } catch (error) {
+      console.error("로그인 중 오류 발생:", error);
+      showToast(TOAST_MSG.LOGIN_ERROR, "error");
+    }
+  };
+
   return (
     <div className="w-screen h-screen relative">
       <MapComponent
@@ -113,23 +165,25 @@ export default function Home() {
         forgetPasswordMsg={LOGIN_MSG.FORGET_PASSWORD_MSG}
         handleForgetPassword={() => {}}
         loginMsg={LOGIN_MSG.LOGIN_BUTTON_MSG}
-        handleLogin={() => {}}
+        handleLogin={loginFunc}
         open={isLoginModalOpen}
         onOpenChange={() => {
-          setIsLoginModalOpen((prev) => !prev)
+          setIsLoginModalOpen((prev) => !prev);
         }}
-      >        
-      </LoginModal>
+      ></LoginModal>
       <SpeedDial
         actions={[
           {
             key: "login",
             icon: <PersonIcon />,
             tooltipTitle: "login",
-            onClick: () => {setIsLoginModalOpen(true)},
+            onClick: () => {
+              setIsLoginModalOpen(true);
+            },
           },
         ]}
       ></SpeedDial>
+      <ToastComponent open={open} setOpen={setOpen} type={type} time={time} msg={msg}/>
     </div>
   );
 }
