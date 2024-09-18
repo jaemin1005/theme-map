025e71e702c605367913d42e7bfef7c52d9db376
jaemin1005 <ghkdwoals1005@gmail.com>
2024-09-18 17:54:54 +0900

    New | ToastComponent를 관리하고 표시하기 위한 useToast 커스텀 훅을 작성
    showToast 함수는 매개변수로 전달받은 메시지, 유형, 지속 시간을 설정하고
    setOpen(true)를 호출하여 토스트를 화면에 나타낸다.

---
+++ web/components/toast/toast_hook.ts
@@ -0,0 +1,54 @@
+import { useState, Dispatch, SetStateAction } from "react";
+import { AlertType } from "./toast_interface";
+
+// 토스트 메세지 시간
+interface UseToastProps {
+  defaultTime?: number;
+}
+
+interface UseToastReturn {
+  open: boolean;
+  msg: string;
+  time: number;
+  type: AlertType;
+  setOpen: Dispatch<SetStateAction<boolean>>;
+
+  /**
+   * 토스트 메세지를 화면에 보여주기 위한 메소드
+   * @param message : 토스트 메세지
+   * @param type : MUI Alert Type
+   * @param duration : 토스트 시간
+   * @returns 
+   */
+  showToast: (message: string, type: AlertType, duration?: number) => void;
+}
+
+/**
+ * 토스트 메세시에 대한 커스텀 훅,
+ * @param param0
+ * @returns
+ */
+export const useToast = ({
+  defaultTime = 3000,
+}: UseToastProps = {}): UseToastReturn => {
+  const [open, setOpen] = useState(false);
+  const [msg, setMsg] = useState("");
+  const [time, setTime] = useState(defaultTime);
+  const [type, setType] = useState<AlertType>("success");
+
+  const showToast = (message: string, type: AlertType, duration?: number) => {
+    setMsg(message);
+    setTime(duration || defaultTime);
+    setType(type);
+    setOpen(true);
+  };
+
+  return {
+    open,
+    msg,
+    time,
+    type,
+    setOpen,
+    showToast,
+  };
+};
