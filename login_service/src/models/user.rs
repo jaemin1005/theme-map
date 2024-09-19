9b8d35f2b59c36e2fed5d9db37e7005565926d7d
jaemin1005 <ghkdwoals1005@gmail.com>
2024-09-19 23:26:54 +0900

    New | 유저에 관련된 모델을 정의하였다
    User : mongodb의 유저에 대한 collection을 정의하였다
    RegisterRequest: 사용자의  회원가입 요청에 대한 구조체를 정의
    LoginRequest: 사용자의 로그인 요청에 대한 구조체를 정의
    LoginResponse: 로그인 성공 시 클라이언트에 반환할 데이터를 정의

---
+++ login_service/src/models/user.rs
@@ -0,0 +1,31 @@
+use mongodb::bson::oid::ObjectId;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct User {
+    #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
+    pub id: Option<ObjectId>,
+    pub name: String,
+    pub email: String,
+    pub password: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct RegisterRequest {
+    pub name: String,
+    pub email: String,
+    pub password: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct LoginRequest {
+    pub email: String,
+    pub password: String,
+}
+
+#[derive(Debug, Serialize)]
+pub struct LoginResponse {
+    pub user: User,
+    pub access_token: String,
+    pub refresh_token: String,
+}
