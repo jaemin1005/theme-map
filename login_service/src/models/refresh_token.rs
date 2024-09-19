cb026dfe3ac2c192f7228c296c480f9d14394526
jaemin1005 <ghkdwoals1005@gmail.com>
2024-09-19 23:37:53 +0900

    Style | 구조체에 대한 주석추가

--- login_service/src/models/refresh_token.rs
+++ login_service/src/models/refresh_token.rs
@@ -2,6 +2,7 @@
 use serde::{Deserialize, Serialize};
 use mongodb::bson::oid::ObjectId;
 
+// MongoDB에서 리프레시 토큰을 관리하기 위해 RefreshToken 구조체를 정의
 #[derive(Debug, Serialize, Deserialize)]
 pub struct RefreshToken {
     #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
