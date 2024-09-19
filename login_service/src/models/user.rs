c6cfd0ec53039170bed057952ee0eed6b758cf7d
jaemin1005 <ghkdwoals1005@gmail.com>
2024-09-19 23:31:01 +0900

    Style | 구조체에 대한 주석 추가

--- login_service/src/models/user.rs
+++ login_service/src/models/user.rs
@@ -1,6 +1,7 @@
 use mongodb::bson::oid::ObjectId;
 use serde::{Deserialize, Serialize};
 
+// 유저 Collection 구조체 정의
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct User {
     #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
@@ -10,6 +11,7 @@ pub struct User {
     pub password: String,
 }
 
+// 회원가입 요청 구조체 정의
 #[derive(Debug, Deserialize)]
 pub struct RegisterRequest {
     pub name: String,
@@ -17,12 +19,14 @@ pub struct RegisterRequest {
     pub password: String,
 }
 
+// 로그인 요정 구조체 정의
 #[derive(Debug, Deserialize)]
 pub struct LoginRequest {
     pub email: String,
     pub password: String,
 }
 
+// 로그인 성공했을 때, 반환할 데이터 정의 (res.body)
 #[derive(Debug, Serialize)]
 pub struct LoginResponse {
     pub user: User,
