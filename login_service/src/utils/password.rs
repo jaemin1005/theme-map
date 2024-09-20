7b3a8696034a76eeba910ad0bc8991b8052847ae
jaemin1005 <ghkdwoals1005@gmail.com>
2024-09-20 13:05:51 +0900

    Style | 비밀번호 해쉬화에 관한 주석을 추가하였다
    추가한 부분은 bcrypt::DEFAULT_COST에 관한 설명을 추가

--- login_service/src/utils/password.rs
+++ login_service/src/utils/password.rs
@@ -1,6 +1,7 @@
 use bcrypt::{hash, verify, BcryptError};
 
 // 패스워드 해쉬로 암호화
+// bcrypt::DEFAULT_COST의 기본값은 12. 이 숫자는 해시 연산을 2^12 (4096)번 반복하여 해시 값을 계산함을 의미
 pub fn hash_password(password: &str) -> Result<String, BcryptError> {
     hash(password, bcrypt::DEFAULT_COST)
 }
@@ -9,3 +10,4 @@ pub fn hash_password(password: &str) -> Result<String, BcryptError> {
 pub fn verify_password(password: &str, hashed_password: &str) -> Result<bool, BcryptError> {
     verify(password, hashed_password)
 }
+
