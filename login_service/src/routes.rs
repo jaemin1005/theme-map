35bb717475a465ff5be6e4d8c0314f4aea050233
jaemin1005 <ghkdwoals1005@gmail.com>
2024-09-20 19:31:03 +0900

    New | 인증 서비스의 앤드포인트와 라우터를 매핑을 구현

---
+++ login_service/src/routes.rs
@@ -0,0 +1,14 @@
+use actix_web::web;
+
+use crate::end_points::auth;
+
+pub fn init(cfg: &mut web::ServiceConfig) {
+    cfg.service(
+        web::scope("/api/auth")
+            .route("/register", web::post().to(auth::register))
+            .route("/login", web::post().to(auth::login))
+            .route("/refresh", web::post().to(auth::refresh))
+            .route("/logout", web::post().to(auth::logout))
+            .route("/me", web::get().to(auth::me)),
+    );
+}
