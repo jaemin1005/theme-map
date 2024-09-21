6d28fd7b58609cee363361ef5648e19bd163fc2a
jaemin1005 <ghkdwoals1005@gmail.com>
2024-09-21 17:05:50 +0900

    Fix | class-validator를 통해 유효성 검사할 수 있게 class로 변경

--- web/interface/register.dto.ts
+++ web/interface/register.dto.ts
@@ -1,5 +1,16 @@
-export interface Register {
-    eamil: string,
-    name: string,
-    password: string,
+import { IsEmail, IsNotEmpty, IsString } from "class-validator";
+
+export class RegisterReq {
+
+    @IsNotEmpty()
+    @IsEmail()
+    email!: string
+
+    @IsNotEmpty()
+    @IsString()
+    name!: string;
+
+    @IsNotEmpty()
+    @IsString()
+    password!: string;
 }
