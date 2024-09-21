3dcc4b7cffb7c2aa0a4f2ee76bcd65ea0d1fda54
jaemin1005 <ghkdwoals1005@gmail.com>
2024-09-21 17:04:54 +0900

    Fix | class-validator를 통해 유효성 검사할 수 있게 class로 변경

--- web/interface/login.dto.ts
+++ web/interface/login.dto.ts
@@ -1,4 +1,11 @@
-export interface Login {
-    email: string,
-    password: string,
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class LoginReq {
+    @IsNotEmpty()
+    @IsEmail()
+    email!: string
+
+    @IsNotEmpty()
+    @IsString()
+    password!: string
 }
