f30d6012626476ee1d450da9a1abf75866190d9a
jaemin1005 <ghkdwoals1005@gmail.com>
2024-09-24 14:23:22 +0900

    Rename | 디렉토리 위치 변경

--- web/static/error_msg.ts
+++
@@ -1,4 +0,0 @@
-export enum ERROR_MSG {
-    GEOLOCATION_GETTING_FAIL = "Error Getting Loaction: ",
-    GEOLOCATION_NOT_AVAILABLE = "Geolocation is not available in your browser"
-}
---
+++ web/static/log/error_msg.ts
@@ -0,0 +1,9 @@
+export enum ERROR_MSG {
+    GEOLOCATION_GETTING_FAIL = "Error Getting Loaction: ",
+    GEOLOCATION_NOT_AVAILABLE = "Geolocation is not available in your browser",
+    
+    //api
+    INTERNAL_SERVER_ERROR = "Internal Server Error",
+    UNAVAILABLE_HTTP_METHOD = "Unavailable Http Method",
+    REGISTER_VALIDATE_FAIL = "Register Validate Fail",
+}
