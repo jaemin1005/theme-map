d034251b8df41e12e3fec1ac3dfcc01c6800a466
jaemin1005 <ghkdwoals1005@gmail.com>
2024-09-24 19:31:15 +0900

    Feat | 로그인 성공 문구 추가

--- web/static/component/toast_msg.ts
+++ web/static/component/toast_msg.ts
@@ -4,5 +4,8 @@ export enum TOAST_MSG {
     INVALID_EMAIL = "Enter a valid email.",
     REGISTER_SUCCESS = "Success Signin.",
     REGISTER_FAIL = "Fail Signin.",
-    VALIDATE_FAIL = "Validation failed."
+    VALIDATE_FAIL = "Validation failed.",
+
+    // 뒤에 문구의 추가가 필요
+    LOGIN_SUCCESS = "Welcome "
 }
