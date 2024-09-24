994f97ec6f16dff1936e649fe3719e988ccbed42
jaemin1005 <ghkdwoals1005@gmail.com>
2024-09-24 14:29:23 +0900

    Feat | 빌드시 eslint 오류 무시 추가

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  eslint: {
+    // 빌드 중 ESLint 오류를 무시하고 계속 진행할지 여부 설정
+    ignoreDuringBuilds: true,
+  },
+};
 
 export default nextConfig;
