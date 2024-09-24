85c57c52b8c04a7a63543f64aa72670c712d94f1
jaemin1005 <ghkdwoals1005@gmail.com>
2024-09-24 14:18:24 +0900

    Feat | Toast Message 추가

--- web/static/toast_msg.ts
+++ web/static/toast_msg.ts
@@ -1,5 +1,8 @@
 export enum TOAST_MSG {
     LOGIN_FAIL = "Incorrect username or password.",
     LOGIN_ERROR = "An error occurred during login.",
-    INVALID_EMAIL = "Enter a valid email."
+    INVALID_EMAIL = "Enter a valid email.",
+    REGISTER_SUCCESS = "Success Signin.",
+    REGISTER_FAIL = "Fail Signin.",
+    VALIDATE_FAIL = "Validation failed."
 }
