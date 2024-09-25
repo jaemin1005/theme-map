216622998f81727e3a6588e9957a37079f12ce2f
jaemin1005 <ghkdwoals1005@gmail.com>
2024-09-25 19:33:15 +0900

    Feat | 유효성검사 추가

--- web/app/api/auth/login/route.ts
+++ web/app/api/auth/login/route.ts
@@ -1,11 +1,32 @@
-import { NextRequest, NextResponse } from 'next/server';
-import cookie from 'cookie';
-import { LoginServiceRes } from '@/interface/login.dto';
+import { NextRequest, NextResponse } from "next/server";
+import cookie from "cookie";
+import { plainToInstance } from "class-transformer";
+import { validateOptions } from "@/static/validate_option";
+import { LoginReq, LoginServiceRes } from "@/interface/login.dto";
+import { validateSync } from "class-validator";
+import { ERROR_MSG } from "@/static/log/error_msg";
+import { convertUser } from "@/Func/convert_user";
 
 // 로그인 핸들러 POST 메서드
 export async function POST(req: NextRequest) {
   try {
-    const { email, password } = await req.json();
+    const requestData = await req.json();
+
+    // 유효성 검사
+    const loginReq = plainToInstance(LoginReq, requestData);
+    const errors = validateSync(loginReq, validateOptions);
+
+    if (errors.length > 0) {
+      return NextResponse.json(
+        {
+          message: ERROR_MSG.REGISTER_VALIDATE_FAIL,
+          details: errors.map((err) => err.toString()),
+        },
+        { status: 400 }
+      );
+    }
+
+    const { email, password } = loginReq;
 
     // Actix Web 서버로 로그인 요청 전송
     const response = await fetch("http://localhost:8080/api/auth/login", {
@@ -17,34 +38,40 @@ export async function POST(req: NextRequest) {
     });
 
     if (response.ok) {
-      const data = await response.json() as LoginServiceRes;
-      
-      // 리프레시 토큰을 HTTP-Only 쿠키로 설정
-      const refreshTokenCookie = cookie.serialize("refreshToken", data.refresh_token, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        // 1주일
-        maxAge: 60 * 60 * 24 * 7,
-        path: "/",
-      });
+      const data = (await response.json()) as LoginServiceRes;
 
-      const user = {
-        id: data.user._id.$oid,
-        email: data.user.email,
-        name: data.user.name
-      }
+      // 리프레시 토큰을 HTTP-Only 쿠키로 설정
+      const refreshTokenCookie = cookie.serialize(
+        "refreshToken",
+        data.refresh_token,
+        {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          // 1주일
+          maxAge: 60 * 60 * 24 * 7,
+          path: "/",
+        }
+      );
+
+      const user = convertUser(data.user);
 
       // NextResponse에 쿠키 설정
-      const res = NextResponse.json({ user, accessToken: data.access_token }, { status: 200 });
+      const res = NextResponse.json(
+        { user, accessToken: data.access_token },
+        { status: 200 }
+      );
       res.headers.append("Set-Cookie", refreshTokenCookie);
 
       return res;
     } else {
       const errorData = await response.json();
-      return NextResponse.json({ message: errorData.message }, { status: response.status });
+      return NextResponse.json(
+        { message: errorData.message },
+        { status: response.status }
+      );
     }
   } catch (error) {
-    console.error("로그인 중 오류 발생:", error);
-    return NextResponse.json({ message: "서버 내부 오류" }, { status: 500 });
+    console.error('Internal Server Error:', error);
+    return NextResponse.json({ message: ERROR_MSG.INTERNAL_SERVER_ERROR }, { status: 500 });
   }
 }
