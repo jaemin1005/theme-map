113ca6b8bc8b058e47ac3ff1fcdff3a7503b30ff
jaemin1005 <ghkdwoals1005@gmail.com>
2024-09-26 20:00:52 +0900

    Feat | 컴포넌트의 스타일 재구성 및 삭제 기능을 추가하였음

--- web/components/modal/marker_info_component.tsx
+++ web/components/modal/marker_info_component.tsx
@@ -1,31 +1,61 @@
 import { fileToUrl } from "@/Func/file_to_url";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
+import { IconButton } from "@mui/material";
+import { Card, CardBody, CardFooter, CardHeader } from "@nextui-org/card";
 import Image from "next/image";
 
 interface MarkerInfoComponent {
   file: File;
   title: string;
   body: string;
+  onClickEdit: React.MouseEventHandler;
+  onClickDelete: React.MouseEventHandler;
 }
 
-export const MarkerInfoComponent: React.FC<MarkerInfoComponent> = ({file, title, body}) => {
+export const MarkerInfoComponent: React.FC<MarkerInfoComponent> = ({
+  file,
+  title,
+  body,
+  onClickEdit,
+  onClickDelete,
+}) => {
   return (
-    <div className="h-[130px] w-full flex flex-row gap-x-2 p-2 shadow-lg">
-      {file && (
-        <Image
-          width={120}
-          height={120}
-          src={fileToUrl(file)}
-          alt="이미지"
-          objectFit="fit"
-          className="rounded-md"
-        ></Image>
+    <Card
+      isFooterBlurred
+      radius="lg"
+      className="border-none relative w-full h-auto"
+    >
+      {file ? (
+        <>
+          <div className="w-full aspect-square">
+            <Image
+              alt="Image"
+              fill={true}
+              className="object-cover aspect-square w-full"
+              src={fileToUrl(file)}
+            />
+          </div>
+          <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
+            <p className="text-large text-white/80">{title}</p>
+          </CardFooter>
+        </>
+      ) : (
+        <CardBody className="pt-7">
+          <h1 className="font-extrabold whitespace-nowrap overflow-x-hidden">
+            {title}
+          </h1>
+          <p className="line-clamp-2 overflow-hidden">{body}</p>
+        </CardBody>
       )}
-      <div className="w-full overflow-hidden">
-        <h2 className="font-extrabold text-medium whitespace-nowrap">
-          {title}
-        </h2>
-        <p className="mt-2 text-wrap">{body}</p>
+      <div className="absolute top-0 right-0 flex z-50">
+        <IconButton onClick={onClickEdit}>
+          <EditIcon/>
+        </IconButton>
+        <IconButton onClick={onClickDelete}>
+          <DeleteIcon/>
+        </IconButton>
       </div>
-    </div>
+    </Card>
   );
 };
