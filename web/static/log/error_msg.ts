569ab0fe00ff80f4537e6e0edab5062d5ba21781
jaemin1005 <ghkdwoals1005@gmail.com>
2024-09-26 12:34:19 +0900

    New | Refresh token, 로그아웃 실패 메세지 추가

--- web/static/log/error_msg.ts
+++ web/static/log/error_msg.ts
@@ -3,7 +3,12 @@ export enum ERROR_MSG {
     GEOLOCATION_NOT_AVAILABLE = "Geolocation is not available in your browser",
     
     //api
+    NO_REFRESH_TOKEN = "Don't have refresh token",
+
+
     INTERNAL_SERVER_ERROR = "Internal Server Error",
     UNAVAILABLE_HTTP_METHOD = "Unavailable Http Method",
     REGISTER_VALIDATE_FAIL = "Register Validate Fail",
+
+    LOGOUT_FAIL = "Logout fail",
 }
