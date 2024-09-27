127eeea00e7b598f6266230308d1ea31a540ab38
jaemin1005 <ghkdwoals1005@gmail.com>
2024-09-27 22:57:12 +0900

    Feat | Map Context Props에 고유 id, 유저Id userid,  제목 title, 설명 body 추가

--- web/context/map_context.tsx
+++ web/context/map_context.tsx
@@ -1,6 +1,12 @@
 "use client";
 
-import { createContext, useState, ReactNode, useContext, useCallback } from "react";
+import {
+  createContext,
+  useState,
+  ReactNode,
+  useContext,
+  useCallback,
+} from "react";
 
 // 각 마크의 상세정보를 담기위한 인터페이스
 export interface Mark {
@@ -11,31 +17,61 @@ export interface Mark {
 }
 
 // context의 타입
-interface MarkInfo {
+interface MapInfo {
+  // Map Info
+  id: string;
+  userId: string;
+  title: string;
+  body: string;
+  setId: (id: string) => void;
+  setUserId: (userId: string) => void;
+  setTitle: (title: string) => void;
+  setBody: (body: string) => void;
+
+  // Mark Info
   marks: Mark[];
   addMark: (mark: Mark) => void;
   delMark: (idx: number) => void;
 }
 
-const MapContext = createContext<MarkInfo | null>(null);
+const MapContext = createContext<MapInfo | null>(null);
 
 export const MapProvider = ({ children }: { children: ReactNode }) => {
+  const [id, setId] = useState<string>("");
+  const [userId, setUserId] = useState<string>("");
+  const [title, setTitle] = useState<string>("");
+  const [body, setBody] = useState<string>("");
+
   const [marks, setMarks] = useState<Mark[]>([]);
 
   const addMark = useCallback((mark: Mark) => {
     setMarks((prev) => [...prev, mark]);
-  },[]);
+  }, []);
 
   const delMark = useCallback((index: number) => {
     setMarks((prev) => {
-      const newMarks = [...prev]; 
-      newMarks.splice(index, 1); 
-      return newMarks; 
+      const newMarks = [...prev];
+      newMarks.splice(index, 1);
+      return newMarks;
     });
   }, []);
 
   return (
-    <MapContext.Provider value={{marks, addMark, delMark }}>
+    <MapContext.Provider
+      value={{
+        id,
+        userId,
+        title,
+        body,
+        setId,
+        setUserId,
+        setTitle,
+        setBody,
+        marks,
+        addMark,
+        delMark,
+      }}
+    >
       {children}
     </MapContext.Provider>
   );
@@ -46,14 +82,14 @@ export const useMap = () => {
   try {
     const context = useContext(MapContext);
     if (context === null) {
-      console.error("useMarkInfo must be used within an MarkProvider");
+      console.error("useMap must be used within an MapProvider");
       throw Error;
     }
 
     return context;
   } catch (err) {
     //! Provider의 외에서 사용할 경우 에러
-    console.error("useMarkInfo must be used within an MarkProvider");
+    console.error("useMap must be used within an MapProvider");
     throw Error;
   }
 };
