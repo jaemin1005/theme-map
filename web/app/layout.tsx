883f654459409be460d7fa177398a7508e0b2064
jaemin1005 <ghkdwoals1005@gmail.com>
2024-09-28 22:21:14 +0900

    Fix | localfont import 제거

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 import { MapProvider } from "@/context/map_context";
 import { AuthProvider } from "@/context/auth_context";
