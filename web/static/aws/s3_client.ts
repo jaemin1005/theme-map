44918511053cfa74744ab8533defd8b3489fb0a2
jaemin1005 <ghkdwoals1005@gmail.com>
2024-09-29 16:53:41 +0900

    Style | export 추가

--- web/static/aws/s3_client.ts
+++ web/static/aws/s3_client.ts
@@ -1,6 +1,6 @@
 import { S3Client } from "@aws-sdk/client-s3";
 
-const s3 = new S3Client({
+export const s3 = new S3Client({
   region: process.env.AWS_REGION,
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
