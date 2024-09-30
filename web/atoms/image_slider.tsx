f94d4f7212a6ace0f041402213839dbec539d1da
jaemin1005 <ghkdwoals1005@gmail.com>
2024-09-30 21:12:02 +0900

    Fix | ImageDatas로 Props로 변경 useImageUrls를 이용해 url를 가져온다

--- web/atoms/image_slider.tsx
+++ web/atoms/image_slider.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import {
   Navigation,
   Pagination,
@@ -14,53 +13,43 @@ import "swiper/css/pagination";
 import "swiper/css/scrollbar";
 import "swiper/css/effect-coverflow";
 import Image from "next/image";
+import { ImageData } from "@/interface/content.dto";
+import { useImageUrls } from "@/utils/use_image_url";
 
 interface ImageSliderProps {
-  blobs: Blob[];
+  imageDatas: ImageData[];
 }
 
-export const ImageSlider: React.FC<ImageSliderProps> = ({ blobs }) => {
-  const [imageUrlList, setImageUrlList] = useState<string[] | null>(null);
-
-  useEffect(() => {
-    const urlList: string[] = [];
-
-    for (const blob of blobs) {
-      urlList.push(URL.createObjectURL(blob));
-    }
-
-    setImageUrlList(urlList);
-
-    return () => {
-      setImageUrlList([]);
-    };
-  }, [blobs]);
+export const ImageSlider: React.FC<ImageSliderProps> = ({ imageDatas }) => {
+  const urls = useImageUrls(imageDatas);
 
   return (
     <div className="w-full min-h-max">
-      <Swiper
-        modules={[Navigation, Pagination, Scrollbar, A11y, EffectCoverflow]}
-        spaceBetween={-10}
-        slidesPerView={1}
-        slidesPerGroup={1}
-        navigation
-        pagination={{ clickable: true }}
-        scrollbar={{ draggable: true }}
-        loop={true}
-        effect="coverflow"
-        centerInsufficientSlides={true}
-      >
-        {imageUrlList?.map((val, key) => (
-          <SwiperSlide key={key} className="w-full relative aspect-square">
-            <Image
-              alt="slider"
-              className="object-cover aspect-square"
-              src={val}
-              fill={true}
-            />
-          </SwiperSlide>
-        ))}
-      </Swiper>
+      {urls.length > 0 && (
+        <Swiper
+          modules={[Navigation, Pagination, Scrollbar, A11y, EffectCoverflow]}
+          spaceBetween={-10}
+          slidesPerView={1}
+          slidesPerGroup={1}
+          navigation
+          pagination={{ clickable: true }}
+          scrollbar={{ draggable: true }}
+          loop={true}
+          effect="coverflow"
+          centerInsufficientSlides={true}
+        >
+          {urls?.map((val, key) => (
+            <SwiperSlide key={key} className="w-full relative aspect-square">
+              <Image
+                alt="slider"
+                className="object-cover aspect-square"
+                src={val}
+                fill={true}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      )}
     </div>
   );
 };
