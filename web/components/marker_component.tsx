efc64b50d556a0198dc874c437a4746acd9fd02d
jaemin1005 <ghkdwoals1005@gmail.com>
2024-09-30 21:11:00 +0900

    Fix | CustomPopup Props 변경으로 인한, blob -> ImageData를 인수로 건네준다

--- web/components/marker_component.tsx
+++ web/components/marker_component.tsx
@@ -10,7 +10,7 @@ const MarkerComponent: React.FC = () => {
       {marks.map((value, idx) => (
         <Marker key={idx} position={value.point}>
           <CustomPopup
-            blob={value.blobs[0]}
+            imageData={value.imageDatas[0]}
             title={value.title}
             body={value.body}
           ></CustomPopup>
