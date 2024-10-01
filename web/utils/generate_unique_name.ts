79f7fb35bdda8248b67edbb7bda0a054b2d7b9cf
jaemin1005 <ghkdwoals1005@gmail.com>
2024-10-01 23:30:40 +0900

    Fix | blob을 이미 Arraybuffer상태로 변경하기 때문에, ArrayBuffer 변경 부분은 제거

--- web/utils/generate_unique_name.ts
+++ web/utils/generate_unique_name.ts
@@ -1,7 +1,8 @@
-import { createHash } from "crypto"; 
+import { createHash } from "crypto";
 
-export async function generateUniqueName(blob: Blob): Promise<string> {
-  const arrayBuffer = await blob.arrayBuffer();
-  const hash = createHash('sha256').update(Buffer.from(arrayBuffer)).digest('hex');
+export function generateUniqueName(arrayBuffer: ArrayBuffer): string {
+  const hash = createHash("sha256")
+    .update(Buffer.from(arrayBuffer))
+    .digest("hex");
   return hash;
 }
