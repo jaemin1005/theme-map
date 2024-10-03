a4af0e430f5cffe9709d2da9dc3f59e9c71dd52b
jaemin1005 <ghkdwoals1005@gmail.com>
2024-10-03 21:16:42 +0900

    Feat | 컬렉션을 정의
    user_id 부분을 추가하였다.

--- content_management_service/src/models/map.rs
+++ content_management_service/src/models/map.rs
@@ -1,5 +1,25 @@
 use serde::{Deserialize, Serialize};
 
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Map {
+    #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
+    pub id: Option<String>,
+    pub user_id: String,
+    pub body: String,
+    pub marks: Vec<Mark>,
+}
+
+impl Map {
+    pub fn new_with_req(user_id: &str, map_save_req: MapSaveReq) -> Self {
+        Self {
+            id:  map_save_req.id,
+            user_id: user_id.to_string(),
+            body: map_save_req.body,
+            marks: map_save_req.marks
+        }
+    } 
+}
+
 #[derive(Debug, Deserialize, Serialize)]
 pub struct MapSaveReq {
     #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
