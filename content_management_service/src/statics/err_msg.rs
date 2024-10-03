63133420efccd769177e4d65bbaba037abe461cf
jaemin1005 <ghkdwoals1005@gmail.com>
2024-10-03 21:18:33 +0900

    Feat | 에러 메서지 정의 추가
    1. find_one 메소드 실패
    2. _id 비교 실패

--- content_management_service/src/statics/err_msg.rs
+++ content_management_service/src/statics/err_msg.rs
@@ -1,1 +1,7 @@
 pub const DB_ERR_MSG: &str = "DB_ERROR";
+
+// _id를 이용해 find_one 메소드가 실패 했을 때
+pub const DB_FIND_FAIL: &str = "DB_FAIL_FIND_ID";
+
+// 1. map의 user_id 와 access_token의 user_id 불일치
+pub const DB_INCORRECT_TOKEN_ID: &str = "DB_INCORRECT_TOKEN_ID";
