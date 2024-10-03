c15549cdecd26c760d1dbccd09f231945abdd021
jaemin1005 <ghkdwoals1005@gmail.com>
2024-10-03 19:40:25 +0900

    New | 맵 저장 기능을 처리하는 컨트롤러 함수 구현
    유저 header에서 id를 가져오고  맵 저장 결과에 따라 응답한다.

---
+++ content_management_service/src/controllers/map.rs
@@ -0,0 +1,28 @@
+use actix_web::{web, HttpRequest, HttpResponse, Responder};
+use mongodb::{Client, Database};
+
+use crate::models::err::ErrorRes;
+use crate::models::map::{MapSaveReq, MapSaveRes};
+use crate::statics::err_msg;
+use crate::utils::get_user_info::get_user_info;
+use crate::services::map_service;
+
+pub async fn map_save(
+  req: HttpRequest,
+  body: web::Json<MapSaveReq>,
+  db: web::Data<Database>,
+  client: web::Data<Client>
+) -> impl Responder {
+    let user_id = match get_user_info(&req){
+      Ok(user_id) => user_id,
+      Err(e) => return HttpResponse::InternalServerError().json(ErrorRes::new(&e.to_string())),
+    };
+
+    match map_service::map_save(body.into_inner(), &user_id, &db, &client).await{
+      Ok(map) => match map.id {
+          Some(id) => HttpResponse::Ok().json(MapSaveRes::new(id)),
+          None => HttpResponse::InternalServerError().json(ErrorRes::new(err_msg::DB_ERR_MSG)),
+      }
+      Err(_) => HttpResponse::InternalServerError().json(ErrorRes::new(err_msg::DB_ERR_MSG))
+    }
+}
