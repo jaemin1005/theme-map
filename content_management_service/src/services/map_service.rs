747f32fed018b5b5fd0656b761e8f1f999a598ca
jaemin1005 <ghkdwoals1005@gmail.com>
2024-10-03 21:19:11 +0900

    Feat | 새로운 컬렉션 구조에 따른 변경과, access_token의 user_id와 맵의 user_id 비교하는 로직 추가

--- content_management_service/src/services/map_service.rs
+++ content_management_service/src/services/map_service.rs
@@ -1,18 +1,24 @@
-use crate::models::map::MapSaveReq;
+use crate::{
+    models::map::{Map, MapSaveReq},
+    statics::err_msg::{DB_FIND_FAIL, DB_INCORRECT_TOKEN_ID},
+};
 use mongodb::{
-    bson::{self, doc}, Client, Database
+    bson::{self, doc},
+    Client, Database,
 };
 
 pub async fn map_save(
-    map: MapSaveReq,
+    map_save_req: MapSaveReq,
     user_id: &str,
     db: &Database,
     client: &Client,
-) -> Result<MapSaveReq, Box<dyn std::error::Error>> {
-    let maps = db.collection::<MapSaveReq>("maps");
+) -> Result<Map, Box<dyn std::error::Error>> {
+    let maps = db.collection::<Map>("maps");
 
     // _id가 없을 경우, 새로운 맵이라고 판단한다.
-    if let None = map.id {
+    if let None = map_save_req.id {
+        let map = Map::new_with_req(user_id, map_save_req);
+
         let insert_result = maps.insert_one(map, None).await?;
 
         let result_id = insert_result
@@ -20,13 +26,24 @@ pub async fn map_save(
             .as_object_id()
             .ok_or("삽입된 ID를 가져오지 못했습니다.")?;
 
-        let map = maps
+        let new_map = maps
             .find_one(doc! {"_id": result_id}, None)
             .await?
             .ok_or("맵을 찾지 못했습니다")?;
 
-        Ok(map)
+        Ok(new_map)
     } else {
+
+        let find_doc = doc! {"_id": map_save_req.id};
+
+        let find_map = maps
+            .find_one(find_doc.clone(), None)
+            .await?
+            .ok_or(DB_FIND_FAIL)?;
+
+        if find_map.user_id != user_id {
+            return Err(DB_INCORRECT_TOKEN_ID.into())
+        }
 
         // session 생성
         let mut session = client.start_session(None).await?;
@@ -36,32 +53,29 @@ pub async fn map_save(
         // 트랜잭션 시작
         session.start_transaction(None).await?;
 
-        let filter = doc! {"_id": map.id};
-
         let unset = doc! {
           "$unset" : { "title": "", "body": "", "marks": "" }
         };
 
-        maps.update_one_with_session(filter.clone(), unset, None, &mut session)
+        maps.update_one_with_session(find_doc.clone(), unset, None, &mut session)
             .await?;
 
-        let marks_bson_value = bson::to_bson(&map.marks)?;
+        let marks_bson_value = bson::to_bson(&map_save_req.marks)?;
 
         let set = doc! {
-          "$set": {"title": map.title, "body": map.body, "marks": marks_bson_value}
+          "$set": {"title": map_save_req.title, "body": map_save_req.body, "marks": marks_bson_value}
         };
 
-        maps.update_one_with_session(filter.clone(), set, None, &mut session)
+        maps.update_one_with_session(find_doc.clone(), set, None, &mut session)
             .await?;
 
         let update_map = maps
-            .find_one_with_session(filter.clone(), None, &mut session)
+            .find_one_with_session(find_doc.clone(), None, &mut session)
             .await?
-            .ok_or("업데이트 된 맵을 찾지 못했습니다")?;
+            .ok_or(DB_FIND_FAIL)?;
 
         session.commit_transaction().await?;
 
         Ok(update_map)
     }
 }
-
