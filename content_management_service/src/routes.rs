1dff565e2035abac2ec37cc8c8bdbf65b2678e6f
jaemin1005 <ghkdwoals1005@gmail.com>
2024-10-04 22:15:39 +0900

    Style | 라우트 경로 수정

--- content_management_service/src/routes.rs
+++ content_management_service/src/routes.rs
@@ -1,10 +1,11 @@
-use actix_web::web;
+use actix_web::{web, HttpResponse, Responder};
 
-use crate::controllers::map;
+
+use crate::{controllers::map, models::map::MapSaveReq};
 
 pub fn init(cfg: &mut web::ServiceConfig) {
     cfg.service(
-        web::scope("/api/contents")
+        web::scope("")
             .route("/map_save", web::post().to(map::map_save))
     );
 }
