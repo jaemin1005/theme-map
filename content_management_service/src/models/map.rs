262e4084ac94a7f208708cefa9c692f4e55921cd
jaemin1005 <ghkdwoals1005@gmail.com>
2024-10-04 21:52:28 +0900

    Fix | id타입 수정 String -> ObjectId

--- content_management_service/src/models/map.rs
+++ content_management_service/src/models/map.rs
@@ -1,10 +1,11 @@
+use mongodb::bson::oid::ObjectId;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Map {
     #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
-    pub id: Option<String>,
-    pub user_id: String,
+    pub id: Option<ObjectId>,
+    pub email: String,
     pub body: String,
     pub marks: Vec<Mark>,
 }
@@ -13,7 +14,7 @@ impl Map {
     pub fn new_with_req(user_id: &str, map_save_req: MapSaveReq) -> Self {
         Self {
             id:  map_save_req.id,
-            user_id: user_id.to_string(),
+            email: user_id.to_string(),
             body: map_save_req.body,
             marks: map_save_req.marks
         }
@@ -23,7 +24,7 @@ impl Map {
 #[derive(Debug, Deserialize, Serialize)]
 pub struct MapSaveReq {
     #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
-    pub id: Option<String>,
+    pub id: Option<ObjectId>,
     pub title: String,
     pub body: String,
     pub marks: Vec<Mark>,
@@ -44,17 +45,16 @@ pub struct ImageData {
     pub isDeleted: bool,
 }
 
-#[derive(Serialize)]
-pub struct MapSaveRes {
-    #[serde(rename = "_id")]
-    pub id: String,
-}
+// #[derive(Serialize)]
+// pub struct MapSaveRes {
+//     #[serde(rename = "_id")]
+//     pub id: ObjectId,
+// }
 
-impl MapSaveRes {
-    pub fn new(id: String) -> Self {
-        Self {
-            id,
-        }
-    }
-}
-
+// impl MapSaveRes {
+//     pub fn new(id: ObjectId) -> Self {
+//         Self {
+//             id,
+//         }
+//     }
+// }
