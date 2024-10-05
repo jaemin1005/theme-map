d697597dcea1b8ebfbee99841b0db8be328f15dc
jaemin1005 <ghkdwoals1005@gmail.com>
2024-10-05 16:41:52 +0900

    Feat | /map_me 앤드포인트와 map_me 함수 연결

--- content_management_service/src/routes.rs
+++ content_management_service/src/routes.rs
@@ -7,5 +7,6 @@ pub fn init(cfg: &mut web::ServiceConfig) {
     cfg.service(
         web::scope("")
             .route("/map_save", web::post().to(map::map_save))
+            .route("/map_me", web::get().to(map::map_me))
     );
 }
