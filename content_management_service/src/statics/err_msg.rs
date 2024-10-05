3b1599e914df134efb0bd070f57cba0944c60e0e
jaemin1005 <ghkdwoals1005@gmail.com>
2024-10-05 16:31:23 +0900

    Feat | 에러 메세지 추가

--- content_management_service/src/statics/err_msg.rs
+++ content_management_service/src/statics/err_msg.rs
@@ -5,3 +5,5 @@ pub const DB_FIND_FAIL: &str = "DB_FAIL_FIND_ID";
 
 // 1. map의 user_id 와 access_token의 user_id 불일치
 pub const DB_INCORRECT_TOKEN_ID: &str = "DB_INCORRECT_TOKEN_ID";
+
+pub const DB_FIND_MAP_FAIL: &str = "DB_FAILD_FIND_MAP";
