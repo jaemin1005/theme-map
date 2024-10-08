e72736cfc8fc78d43223eae817dcb16e32e02a40
jaemin1005 <ghkdwoals1005@gmail.com>
2024-10-08 12:52:17 +0900

    Feat | isEdited 상태에 따라, 수정, 삭제버튼 on/off 여부 추가

--- web/components/modal/map_info_component.tsx
+++ web/components/modal/map_info_component.tsx
@@ -1,4 +1,3 @@
-import { ImageData } from "@/interface/content.dto";
 import DeleteIcon from "@mui/icons-material/Delete";
 import EditIcon from "@mui/icons-material/Edit";
 import { Chip, IconButton } from "@mui/material";
@@ -8,7 +7,7 @@ import FavoriteIcon from "@mui/icons-material/Favorite";
 interface MapsInfoComponentProps {
   title: string;
   body: string;
-  onClick: React.MouseEventHandler,
+  isEdited: boolean;
   onClickEdit: React.MouseEventHandler;
   onClickDelete: React.MouseEventHandler;
 }
@@ -16,16 +15,16 @@ interface MapsInfoComponentProps {
 export const MapInfoComponent: React.FC<MapsInfoComponentProps> = ({
   title,
   body,
-  onClick,
+  isEdited,
   onClickEdit,
   onClickDelete,
 }) => {
+
   return (
     <Card
       isFooterBlurred
       radius="lg"
       className="border-none relative w-full h-min"
-      onClick={onClick}
     >
       <CardBody className="pt-7">
         <h1 className="font-extrabold whitespace-nowrap overflow-x-hidden">
@@ -36,14 +35,16 @@ export const MapInfoComponent: React.FC<MapsInfoComponentProps> = ({
       <CardFooter>
         <Chip icon={<FavoriteIcon />} label="+9999" />
       </CardFooter>
-      <div className="absolute top-0 right-0 flex z-50">
-        <IconButton onClick={onClickEdit}>
-          <EditIcon />
-        </IconButton>
-        <IconButton onClick={onClickDelete}>
-          <DeleteIcon />
-        </IconButton>
-      </div>
+      {isEdited && (
+        <div className="absolute top-0 right-0 flex z-50">
+          <IconButton onClick={onClickEdit}>
+            <EditIcon />
+          </IconButton>
+          <IconButton onClick={onClickDelete}>
+            <DeleteIcon />
+          </IconButton>
+        </div>
+      )}
     </Card>
   );
 };
