76cd0b4680202cbaf123f4741be2e631921aba00
jaemin1005 <ghkdwoals1005@gmail.com>
2024-10-08 09:44:25 +0900

    Fix | endpoint 경로 수정

--- image_service/src/routes.rs
+++ image_service/src/routes.rs
@@ -6,6 +6,6 @@ use crate::controllers::upload;
 pub fn init(cfg: &mut web::ServiceConfig) {
     cfg.service(
         web::scope("")
-            .route("/map_save", web::post().to(upload::upload_images))
+            .route("/upload", web::post().to(upload::upload_images))
     );
 }
