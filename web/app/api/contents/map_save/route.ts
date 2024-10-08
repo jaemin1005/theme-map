2152814ab3a415d02259e520b35265405aa802f1
jaemin1005 <ghkdwoals1005@gmail.com>
2024-10-08 11:42:00 +0900

    Fix | 더이상 formData를 받지 않고, json으로 보내주기 떄문에 수정하였음

--- web/app/api/contents/map_save/route.ts
+++ web/app/api/contents/map_save/route.ts
@@ -1,18 +1,11 @@
 // app/api/save-map/route.ts
 
 import { NextRequest, NextResponse } from "next/server";
-import { parseFormDataToMapSaveReq } from "@/utils/parser_formdata";
-import { ImageData, MapSaveReq, Mark } from "@/interface/content.dto";
+import { MapSaveReq } from "@/interface/content.dto";
 import { plainToInstance } from "class-transformer";
 import { validateOptions } from "@/static/validate_option";
 import { validateSync } from "class-validator";
 import { ERROR_MSG } from "@/static/log/error_msg";
-import { generateUniqueName } from "@/utils/generate_unique_name";
-import {
-  DeleteObjectCommand,
-  PutObjectCommand,
-} from "@aws-sdk/client-s3";
-import { s3 } from "@/static/aws/s3_client";;
 
 // https://stackoverflow.com/questions/66674834/how-to-read-formdata-in-nextjs
 export async function POST(req: NextRequest) {
@@ -25,8 +18,7 @@ export async function POST(req: NextRequest) {
     );
   }
 
-  const formData = await req.formData();
-  const body = await parseFormDataToMapSaveReq(formData);
+  const body = await req.json();
 
   const mapSaveReq = plainToInstance(MapSaveReq, body);
   const errors = validateSync(mapSaveReq, validateOptions);
@@ -40,85 +32,19 @@ export async function POST(req: NextRequest) {
       { status: 400 }
     );
   }
-  const mark: Mark[] = await Promise.all(
-    body.marks.map(async (mark) => {
-      // {url: string, isNew: boolean, isDeleted: boolean}
-      const data: ImageData[] = (
-        await Promise.all(
-          mark.imageDatas.map(async (imageData, index) => {
-            if (imageData.isNew && imageData.blob) {
-              try {
-                const arrayBuffer = await imageData.blob.arrayBuffer();
 
-                const uniqueFileName = await generateUniqueName(arrayBuffer);
+  const response = await fetch('http://localhost:3001/map_save', {
+    method: 'POST',
+    headers: {
+      'Authorization': `${authHeader}`, // 액세스 토큰을 헤더에 포함
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(body),
+  });
 
-                const uploadParams = {
-                  Bucket: process.env.AWS_S3_BUCKET_NAME,
-                  Key: `images/${uniqueFileName}-${index}`,
-                  Body: Buffer.from(arrayBuffer),
-                  ContentType: "image/webp",
-                };
+  console.log(response.status);
+  
+  const data = await response.json();
 
-                const command = new PutObjectCommand(uploadParams);
-                await s3.send(command);
-
-                // 업로드된 파일의 URL 생성
-                const imgUrl = `https://${process.env.AWS_S3_BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com/images/${uniqueFileName}-${index}`;
-                return {
-                  url: imgUrl,
-                  isNew: imageData.isNew,
-                  isDeleted: imageData.isDeleted,
-                };
-              } catch (error) {
-                console.error("S3 업로드 중 오류 발생:", error);
-                throw new Error()
-              }
-            } else if (imageData.isDeleted && imageData.url) {
-              try {
-                // URL에서 Bucket 이름과 Key 추출
-                // 예: 'mybucket.s3.region.amazonaws.com'에서 'mybucket' 추출
-                // 예: '/uploads/uniqueFileName-0.webp'에서 'uploads/uniqueFileName-0.webp' 추출
-                const url = new URL(imageData.url);
-                const bucketName = url.hostname.split(".")[0];
-                const key = decodeURIComponent(url.pathname.substring(1));
-
-                const deleteParams = {
-                  Bucket: bucketName,
-                  Key: key,
-                };
-
-                const deleteCommand = new DeleteObjectCommand(deleteParams);
-                await s3.send(deleteCommand);
-
-                console.log(
-                  `Deleted S3 object: ${key} from bucket: ${bucketName}`
-                );
-
-                return {
-                  url: imageData.url,
-                  isNew: imageData.isNew,
-                  isDeleted: imageData.isDeleted,
-                };
-              } catch (error) {
-                console.error(
-                  `Error deleting S3 object: ${imageData.url}`,
-                  error
-                );
-                throw new Error("Failed to delete image from S3");
-              }
-            }
-          })
-        )
-      ).filter((data) => data !== undefined);
-
-      return {
-        imageDatas: data,
-        title: mark.title,
-        body: mark.body,
-        point: mark.point,
-      };
-    })
-  );
-
-  return NextResponse.json({ message: "맵 저장 완료" }, { status: 200 });
+  return NextResponse.json(data, { status: response.status });
 }
