2ba7a68b983f3c40113bc89676dd2b41b8c79d5b
jaemin1005 <ghkdwoals1005@gmail.com>
2024-10-08 11:58:14 +0900

    Feat | 이미지 허용 설정 추가

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ['motoma.s3.amazonaws.com'], // 이곳에 S3 도메인을 추가합니다.
+  },
   eslint: {
     // 빌드 중 ESLint 오류를 무시하고 계속 진행할지 여부 설정
     ignoreDuringBuilds: true,
