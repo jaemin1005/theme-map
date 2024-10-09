8195175b2f2961056dd141f34f0272da9bab33c4
jaemin1005 <ghkdwoals1005@gmail.com>
2024-10-09 21:20:54 +0900

    New | api 경로를 enum (열거형) 으로 정의

---
+++ web/static/routes.ts
@@ -0,0 +1,18 @@
+export enum API_ROUTE {
+
+  // AUTH
+  REGISTER = "/api/auth/register",
+  LOGIN = "/api/auth/login",
+  LOGOUT = "/api/auth/logout",
+  ME = "/api/auth/me",
+  REPUBLISH_REFRESH_TOKEN = "/api/auth/refresh",
+  REPUBLISH_ACCESS_TOKEN = "/api/auth/refresh/access_token",
+
+  // CONTENTS
+  MAP_SAVE = "/api/contents/map_save",
+  MAP_ME = "/api/contents/me",
+  MAP_READ = "/api/contents/read",
+
+  // UPLOAD
+  UPLOAD = "/api/upload"
+}
