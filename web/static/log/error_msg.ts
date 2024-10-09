a846d56e498fe31a9a0d64d4c40aaff5bb484b79
jaemin1005 <ghkdwoals1005@gmail.com>
2024-10-09 21:28:44 +0900

    Feat | GPS에 대한 에러메세지를 추가하였다

--- web/static/log/error_msg.ts
+++ web/static/log/error_msg.ts
@@ -1,7 +1,11 @@
 export enum ERROR_MSG {
+    // gps에 관련된 
     GEOLOCATION_GETTING_FAIL = "Error Getting Loaction: ",
     GEOLOCATION_NOT_AVAILABLE = "Geolocation is not available in your browser",
-    
+    GEOLOCATION_PERMISSION_DENIED = "User denied the request for Geolocation",
+    GEOLOCATION_POSITION_UNAVAILABLE = "Location information is unavailable",
+    GEOLOCATION_TIMEOUT = "The request to get user location timed out",
+
     //api
     NO_REFRESH_TOKEN = "Don't have refresh token",
 
