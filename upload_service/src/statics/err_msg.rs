ba88687f8f96eb1c42d8804e0beb45b7f76535a0
jaemin1005 <ghkdwoals1005@gmail.com>
2024-10-11 22:04:22 +0900

    Feat | 유효하지 않는 S3 Url주소에 대한 에러 문구 추가

--- upload_service/src/statics/err_msg.rs
+++ upload_service/src/statics/err_msg.rs
@@ -1,2 +1,3 @@
 pub const FILE_NO_METADATA: &str = "File don't have MeteData";
 pub const FILE_NO_FILENAME: &str = "File don't have FileName";
+pub const INVALID_URL_FORMAT: &str = "Invalid url format";
