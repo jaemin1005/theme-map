226b097e51054a750121c9c21cd6f1b6d2f35b6d
jaemin1005 <ghkdwoals1005@gmail.com>
2024-10-11 15:54:24 +0900

    Refactoring | login api route 리팩토링
    유효성 검사 로직 분리, service 주소를 환경변수를 이용해 가져오는걸로 변경

--- web/app/api/auth/login/route.ts
+++ web/app/api/auth/login/route.ts
@@ -1,63 +1,39 @@
 import { NextRequest, NextResponse } from "next/server";
-import cookie from "cookie";
-import { plainToInstance } from "class-transformer";
 import { validateOptions } from "@/static/validate_option";
 import { LoginReq, LoginServiceRes } from "@/interface/auth.dto";
-import { validateSync } from "class-validator";
-import { ERROR_MSG } from "@/static/log/error_msg";
+import { FAILED_VALIDATE_BODY, INTERNAL_SERVER_ERROR } from "@/static/api/res";
+import { validateBody } from "@/utils/api/validate_body";
+import { GET_ENV, getEnv } from "@/utils/api/get_env";
+import { setUpRefreshToken } from "@/utils/api/set_cookie";
 
 // 로그인 핸들러 POST 메서드
 export async function POST(req: NextRequest) {
+  const body = await validateBody(req, LoginReq, validateOptions);
+  if (Array.isArray(body)) {
+    return FAILED_VALIDATE_BODY;
+  }
+
+  const AUTH_SERVICE_URL = getEnv(GET_ENV.AUTH_SERVICE_URL);
+
   try {
-    const requestData = await req.json();
-
-    // 유효성 검사
-    const loginReq = plainToInstance(LoginReq, requestData);
-    const errors = validateSync(loginReq, validateOptions);
-
-    if (errors.length > 0) {
-      return NextResponse.json(
-        {
-          message: ERROR_MSG.REGISTER_VALIDATE_FAIL,
-          details: errors.map((err) => err.toString()),
-        },
-        { status: 400 }
-      );
-    }
-
-    const { email, password } = loginReq;
-
     // Actix Web 서버로 로그인 요청 전송
-    const response = await fetch("http://localhost:8080/api/auth/login", {
+    const response = await fetch(AUTH_SERVICE_URL + "/api/auth/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ email, password }),
+      body: JSON.stringify(body),
     });
 
     if (response.ok) {
       const data = (await response.json()) as LoginServiceRes;
 
-      // 리프레시 토큰을 HTTP-Only 쿠키로 설정
-      const refreshTokenCookie = cookie.serialize(
-        "refreshToken",
-        data.refresh_token,
-        {
-          httpOnly: true,
-          secure: process.env.NODE_ENV === "production",
-          // 1주일
-          maxAge: 60 * 60 * 24 * 7,
-          path: "/",
-        }
-      );
-
-      // NextResponse에 쿠키 설정
       const res = NextResponse.json(
         { user: data.user, accessToken: data.access_token },
         { status: 200 }
       );
-      res.headers.append("Set-Cookie", refreshTokenCookie);
+
+      setUpRefreshToken(res, data.refresh_token);
 
       return res;
     } else {
@@ -68,7 +44,7 @@ export async function POST(req: NextRequest) {
       );
     }
   } catch (error) {
-    console.error('Internal Server Error:', error);
-    return NextResponse.json({ message: ERROR_MSG.INTERNAL_SERVER_ERROR }, { status: 500 });
+    console.error("Internal Server Error:", error);
+    return INTERNAL_SERVER_ERROR;
   }
 }
