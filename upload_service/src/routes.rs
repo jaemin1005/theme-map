503440bb8173f78d54435a0bc3d5e8741ae561de
jaemin1005 <ghkdwoals1005@gmail.com>
2024-10-11 22:37:23 +0900

    Feat | controllers와 /delete 엔드포인트 연결

--- upload_service/src/routes.rs
+++ upload_service/src/routes.rs
@@ -7,5 +7,6 @@ pub fn init(cfg: &mut web::ServiceConfig) {
     cfg.service(
         web::scope("")
             .route("/upload", web::post().to(upload::upload_images))
+            .route("/delete", web::post().to(upload::remove_images))
     );
 }
