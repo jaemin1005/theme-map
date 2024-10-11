dd8491b04a3c4243fea6e878b404ca7d461b1039
jaemin1005 <ghkdwoals1005@gmail.com>
2024-10-11 17:00:55 +0900

    Feat | Refresh 토큰 초기화 로직 추가

--- web/utils/api/set_cookie.ts
+++ web/utils/api/set_cookie.ts
@@ -4,17 +4,26 @@ import { serialize } from "cookie";
 // 1주일
 const TIME = 60 * 60 * 24 * 7;
 
+export const REFRESH_TOKEN = "refreshToken";
+
 export const setUpRefreshToken = (res: NextResponse, refreshToken: string) => {
-  const refreshTokenCookie = serialize(
-    "refreshToken",
-    refreshToken,
-    {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      maxAge: TIME,
-      path: "/",
-    }
-  );
+  const refreshTokenCookie = serialize(REFRESH_TOKEN, refreshToken, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    maxAge: TIME,
+    path: "/",
+  });
+
+  res.headers.append("Set-Cookie", refreshTokenCookie);
+};
+
+export const initRefreshToken = (res: NextResponse) => {
+  const refreshTokenCookie = serialize(REFRESH_TOKEN, "", {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    expires: new Date(0),
+    path: "/",
+  });
 
   res.headers.append("Set-Cookie", refreshTokenCookie);
 }
