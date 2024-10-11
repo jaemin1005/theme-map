0ae59fb8fdc522ac62595c6230ab582c54e42353
jaemin1005 <ghkdwoals1005@gmail.com>
2024-10-11 15:34:01 +0900

    Style | lint 적용

--- web/utils/api/validate_body.ts
+++ web/utils/api/validate_body.ts
@@ -4,18 +4,22 @@ import { NextRequest } from "next/server";
 
 /**
  * req, body의 validate 검사
- * @param req : 요청 
+ * @param req : 요청
  * @param classType : 어떤 classType으로 검사할 것인가?
  * @param validateOptions : 유효성검사 옵션
- * @returns 
+ * @returns
  */
-export const validateBody = async <T extends Object>(req: NextRequest, classType: new () => T, validateOptions: ValidatorOptions): Promise<T | string[]> => {
+export const validateBody = async <T extends Object>(
+  req: NextRequest,
+  classType: new () => T,
+  validateOptions: ValidatorOptions
+): Promise<T | string[]> => {
   const body = (await req.json()) as T;
 
   const instance = plainToInstance(classType, body);
   const errors = await validate(instance, validateOptions);
 
-  if(errors.length > 0){
+  if (errors.length > 0) {
     return errors.map((err) => err.toString());
   }
 
