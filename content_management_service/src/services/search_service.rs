3b9164bad92348a62009dd5c8ec9dac731492087
jaemin1005 <ghkdwoals1005@gmail.com>
2024-10-12 17:38:19 +0900

    New | 맵에 대한 검색 로직 구현
    함수는 사용자가 제공한 SearchType에 따라 MongoDB에서 맵 정보를 검색하고, 검색된 결과를 반환
    access_token의 user_id와 맵의 user_id 를 확인하여 편집 가능 여부를 판단

---
+++ content_management_service/src/services/search_service.rs
@@ -0,0 +1,73 @@
+use mongodb::{
+    bson::{doc, oid::ObjectId},
+    options::FindOptions,
+    Database,
+};
+
+use crate::models::{
+    map::{Map, MapSearchRes},
+    search_model::SearchType,
+    user::User,
+};
+
+use futures_util::TryStreamExt;
+use std::time::Duration;
+
+pub async fn search_map(
+    search_type: &SearchType,
+    body: &str,
+    user_id: Option<&str>,
+    db: &Database,
+) -> Result<Vec<MapSearchRes>, Box<dyn std::error::Error>> {
+    let maps = db.collection::<Map>("maps");
+    let users = db.collection::<User>("users");
+
+    // Optional user_id를 ObjectId로 변환
+    let object_id_user = if let Some(user_id) = user_id {
+        Some(ObjectId::parse_str(user_id)?)
+    } else {
+        None
+    };
+
+    // 검색 조건 설정
+    let find_doc = match search_type {
+        SearchType::TITLE => {
+            doc! {"title": { "$regex": body, "$options": "i" }}
+        } 
+        SearchType::USER => {
+            let user = users
+                .find_one(doc! {"name": { "$regex": body, "$options": "i" }}, None)
+                .await?
+                .ok_or("User not found")?;
+            doc! {"user_id": user.id}
+        }
+    };
+
+    let options = FindOptions::builder()
+        .max_time(Duration::from_secs(10))
+        .build();
+    // Map 문서 검색
+    let mut cursor = maps.find(find_doc, options).await?;
+
+    let mut find_maps: Vec<Map> = Vec::new();
+    while let Some(map) = cursor.try_next().await? {
+        find_maps.push(map);
+    }
+
+    // 검색된 Map을 MapSearchRes로 변환하여 반환
+    let res: Vec<MapSearchRes> = find_maps
+        .into_iter()
+        .map(|map| {
+            let id = map.id.ok_or("Map ID is missing")?;
+            let is_edit = object_id_user.map_or(false, |obj_id| obj_id == map.user_id);
+            Ok(MapSearchRes {
+                id,
+                title: map.title.clone(),
+                body: map.body.clone(),
+                is_edit,
+            })
+        })
+        .collect::<Result<_, Box<dyn std::error::Error>>>()?;
+
+    Ok(res)
+}
