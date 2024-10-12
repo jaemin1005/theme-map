12c9565f824739527e04de634029ee28aa09f1f5
jaemin1005 <ghkdwoals1005@gmail.com>
2024-10-12 17:23:59 +0900

    Style | 구조체의 속정에 접근하기 위해 속성들 public 으로 설정

--- content_management_service/src/models/user.rs
+++ content_management_service/src/models/user.rs
@@ -4,8 +4,8 @@ use mongodb::bson::oid::ObjectId;
 #[derive(Debug, Deserialize)]
 pub struct User {
     #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
-    id: Option<ObjectId>,
-    name: String,
+    pub id: Option<ObjectId>,
+    pub name: String,
     pub email: String,
     password: String,
 }
