a787f5d4b3050271ea03da81d5eb96954d7f5fcc
jaemin1005 <ghkdwoals1005@gmail.com>
2024-10-12 17:21:56 +0900

    New | Search Api router, Client에서 받은 쿼리스트링을 해당 서비스로 uri를 재구성하여 Http Protocol 수행

---
+++ web/app/api/contents/search/route.ts
@@ -0,0 +1,28 @@
+import { INTERNAL_SERVER_ERROR } from '@/static/api/res';
+import { GET_ENV, getEnv } from '@/utils/api/get_env';
+import { NextResponse } from 'next/server';
+
+export async function GET(req: Request) {
+  const { searchParams } = new URL(req.url);
+  const search_type = searchParams.get('search_type');
+  const body = searchParams.get('body');
+
+  const CONTENT_SERVICE_URL = getEnv(GET_ENV.CONTENT_SERVICE_URL);
+  
+  const url = `${CONTENT_SERVICE_URL}/search?search_type=${search_type}&body=${body}`;
+
+  try {
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'Authorization': req.headers.get('Authorization') || '',
+      },
+    });
+
+    const data = await response.json();
+    return NextResponse.json(data, { status: response.status });
+  } catch (error) {
+    console.error(error);
+    return INTERNAL_SERVER_ERROR
+  }
+}
