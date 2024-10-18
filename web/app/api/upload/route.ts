5454878d607529b319a8869a24332b889dbb1bd7
jaemin1005 <ghkdwoals1005@gmail.com>
2024-10-18 16:22:50 +0900

    Fix | Next14 부터는 config Option 사용 x

--- web/app/api/upload/route.ts
+++ web/app/api/upload/route.ts
@@ -3,12 +3,6 @@ import { GET_ENV, getEnv } from "@/utils/api/get_env";
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
-export const config = {
-  api: {
-    bodyParser: false, // 스트림 방식으로 보낼 것이므로 bodyParser를 비활성화
-  },
-};
-
 export async function POST(req: NextRequest) {
   const formData = await req.formData();
 
