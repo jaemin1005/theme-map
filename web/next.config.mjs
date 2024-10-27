fc08e21550b4283b72be405f23aebda565c5ebc9
jaemin1005 <ghkdwoals1005@gmail.com>
2024-10-27 19:02:49 +0900

    Test | nextconfig에 env 설정 없을떄

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -7,11 +7,6 @@ const nextConfig = {
     // 빌드 중 ESLint 오류를 무시하고 계속 진행할지 여부 설정
     ignoreDuringBuilds: true,
   },
-  env: {
-    AUTH_SERVICE_URL: process.env.AUTH_SERVICE_URL,
-    CONTENT_SERVICE_URL: process.env.CONTENT_SERVICE_URL,
-    UPLOAD_SERVICE_URL: process.env.UPLOAD_SERVICE_URL
-  },
   reactStrictMode: false,
 };
 
