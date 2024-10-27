54f9efd82d8ba75dd506a7fb58080d089b825c42
jaemin1005 <ghkdwoals1005@gmail.com>
2024-10-27 20:33:03 +0900

    Feat | 크레이트를 통해 요청,응답 로그 기록

--- upload_service/src/main.rs
+++ upload_service/src/main.rs
@@ -1,7 +1,8 @@
-use actix_web::{web, App, HttpServer};
+use actix_web::{middleware::Logger, web, App, HttpServer};
 use aws_config::BehaviorVersion;
 use aws_sdk_s3::Client;
 use dotenv::dotenv;
+use env_logger::Env;
 
 pub mod controllers {
     pub mod upload;
@@ -29,6 +30,7 @@ pub mod routes;
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
+    env_logger::init_from_env(Env::default().default_filter_or("info"));
 
     //세션을 시작하기 위한 client 생성 ()
     let config = aws_config::defaults(BehaviorVersion::latest()).load().await;
@@ -37,9 +39,10 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(move || {
         App::new()
             .app_data(web::Data::new(s3_client.clone()))
+            .wrap(Logger::default())
             .configure(routes::init)
     })
-    .bind("127.0.0.1:3002")?
+    .bind("0.0.0.0:3002")?
     .run()
     .await
 }
