5e909cee9086863cf97d8a17e76c50577a0dd222
jaemin1005 <ghkdwoals1005@gmail.com>
2024-10-27 17:01:40 +0900

    Fix | 서버에 로그 기록 크레이트를 통해 기능 추가

--- auth_service/src/main.rs
+++ auth_service/src/main.rs
@@ -1,24 +1,26 @@
 use std::io;
 
-use actix_web::{web, App, HttpServer};
+use actix_web::{middleware::Logger, web, App, HttpServer};
 use dotenv::dotenv;
+use env_logger::Env;
 
 mod routes;
 mod end_points {
     pub mod auth;
+    pub mod index;
 }
 mod services {
     pub mod auth_service;
 }
 mod models {
-    pub mod user;
-    pub mod refresh_token;
     pub mod err;
+    pub mod refresh_token;
+    pub mod user;
 }
 mod utils {
+    pub mod db;
     pub mod jwt;
     pub mod password;
-    pub mod db;
     pub mod user;
 }
 
@@ -26,22 +28,24 @@ mod statics {
     pub mod err_msg;
 }
 
-
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     // .env 파일에서 환경 변수를 로드
     dotenv().ok();
 
+    env_logger::init_from_env(Env::default().default_filter_or("info"));
+
     // 데이터베이스 연결을 가져온다.
     let db = match utils::db::get_database().await {
-        Ok(database) => database, 
+        Ok(database) => database,
         Err(e) => {
             eprintln!("Error connecting to the database: {}", e);
-            return Err(io::Error::new(io::ErrorKind::Other, "Database connection failed"));
+            return Err(io::Error::new(
+                io::ErrorKind::Other,
+                "Database connection failed",
+            ));
         }
     };
-
-    println!("Starting server at http://127.0.0.1:8080");
 
     // HTTP 서버를 시작
     // 데이터베이스 연결을 공유
@@ -49,9 +53,11 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(move || {
         App::new()
             .app_data(web::Data::new(db.clone()))
+            .wrap(Logger::default())
             .configure(routes::init)
     })
-    .bind("127.0.0.1:8080")?
+    // 모든 IP에서 접근 가능하도록 설정
+    .bind("0.0.0.0:8080")?
     .run()
     .await
 }
