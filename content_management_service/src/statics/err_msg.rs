77b08dfe1c679442d310c28b560ca1da7e52de6a
jaemin1005 <ghkdwoals1005@gmail.com>
2024-11-10 22:46:56 +0900

    Feat | 에러 메세지를 추가하였다.
    1. 맵이 ID를 가지고 있지 않을 때
    2. 이미지 경로의 파일이름을 찾을 수 없을 때
    3. 잘못된 접근일 때

--- content_management_service/src/statics/err_msg.rs
+++ content_management_service/src/statics/err_msg.rs
@@ -7,3 +7,9 @@ pub const DB_FIND_FAIL: &str = "DB_FAIL_FIND_ID";
 pub const DB_INCORRECT_TOKEN_ID: &str = "DB_INCORRECT_TOKEN_ID";
 
 pub const DB_FIND_MAP_FAIL: &str = "DB_FAILD_FIND_MAP";
+
+pub const MAP_NEED_ID: &str = "MAP_DONT_HAVE_ID";
+
+pub const NOT_FILE_NAME_FOUND: &str = "NOT_FILE_NAME_FOUND";
+
+pub const INCORRECT_ACCESS: &str = "INCORRECT_ACCESS";
