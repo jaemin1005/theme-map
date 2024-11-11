0484d1a5f8471cf43654ad739e88d26a1b40d842
jaemin1005 <ghkdwoals1005@gmail.com>
2024-11-11 22:43:15 +0900

    Fix | 파일이름이 아닌 URL 인코딩을 통해 파일을 접근 및 복사한다

--- content_management_service/src/utils/s3.rs
+++ content_management_service/src/utils/s3.rs
@@ -1,4 +1,5 @@
 use aws_sdk_s3::Client;
+use percent_encoding::{utf8_percent_encode, NON_ALPHANUMERIC};
 
 // s3에서의 파일 복사
 // src -> dst의 경로로 파일을 복사한다
@@ -11,11 +12,12 @@ pub async fn copy_s3(
 ) -> Result<(), Box<dyn std::error::Error>> {
     //! 기존의 temp 파일의 삭제는 S3 규칙에 의해 하루마다 삭제되게 한다
 
-    let copy_key = format!("{}/{}", src_bucket_name, src_key);
+    let encoded_src_key = utf8_percent_encode(src_key, NON_ALPHANUMERIC).to_string();
+    let copy_source = format!("{}/{}", src_bucket_name, encoded_src_key);
 
     s3_client
         .copy_object()
-        .copy_source(copy_key)
+        .copy_source(copy_source)
         .bucket(dst_bucket_name)
         .key(dst_key)
         .send()
