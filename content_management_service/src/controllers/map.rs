e5e18a91c742972e887c751f4e94a1def7ba279b
jaemin1005 <ghkdwoals1005@gmail.com>
2024-11-11 19:09:18 +0900

    Fix | map_save 메소드에서, 추가적으로 인수로 s3 client 인스턴스를 가져온다

--- content_management_service/src/controllers/map.rs
+++ content_management_service/src/controllers/map.rs
@@ -13,13 +13,14 @@ pub async fn map_save(
     req: HttpRequest,
     body: web::Json<MapSaveReq>,
     db: web::Data<Database>,
+    s3_client: web::Data<Client>,
 ) -> impl Responder {
     let user_id = match get_user_info(&req) {
         Ok(user_id) => user_id,
         Err(e) => return HttpResponse::InternalServerError().json(ErrorRes::new(&e.to_string())),
     };
 
-    match map_service::map_save(body.into_inner(), &user_id, &db).await {
+    match map_service::map_save(body.into_inner(), &user_id, &db, &s3_client).await {
         Ok(map) => match map.id {
             Some(id) => HttpResponse::Ok().json(id),
             None => HttpResponse::InternalServerError().json(ErrorRes::new(err_msg::DB_ERR_MSG)),
