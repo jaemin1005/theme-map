c939df95dbc12a55049728ac0aa7303ef892adf5
jaemin1005 <ghkdwoals1005@gmail.com>
2024-11-13 22:48:48 +0900

    Feat | mongoDB 문서에 device_id를 저장하는 속성을 하나 추가하였다

--- auth_service/src/models/refresh_token.rs
+++ auth_service/src/models/refresh_token.rs
@@ -8,6 +8,7 @@ pub struct RefreshToken {
     #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
     pub id: Option<ObjectId>, // None일 경우 직렬화 스킵
     pub user_id: ObjectId,
+    pub device_id: String,
     pub token: String,
     pub expiry: chrono::DateTime<chrono::Utc>,
 }
