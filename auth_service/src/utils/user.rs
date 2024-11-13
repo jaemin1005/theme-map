606b0438f9108f6d4e024691da6474b5412c16ca
jaemin1005 <ghkdwoals1005@gmail.com>
2024-11-13 22:46:17 +0900

    Feat | 헤더에서 Device-ID 추출하는 get_user_device_id 함수 추가

--- auth_service/src/utils/user.rs
+++ auth_service/src/utils/user.rs
@@ -1,8 +1,28 @@
-use crate::models::user::{User, UserRes};
+use actix_web::HttpRequest;
+
+use crate::models::{
+    app_err::AppError,
+    user::{User, UserRes},
+};
 
 pub fn user_to_user_res(user: User) -> UserRes {
-  UserRes {
-    email: user.email,
-    name: user.name
-  }
+    UserRes {
+        email: user.email,
+        name: user.name,
+    }
+}
+
+pub fn get_user_device_id(req: &HttpRequest) -> Result<String, AppError> {
+    // Authorization 헤더에서 액세스 토큰 추출
+    let device_id = req
+        .headers()
+        .get("Device-ID")
+        .ok_or(AppError::InvalidDeviceIdHeader)?
+        .to_str()?;
+
+    if device_id == "" {
+        return Err(AppError::InvalidDeviceIdHeader);
+    }
+
+    Ok(device_id.to_string())
 }
