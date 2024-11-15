18de7ed9f67e3303c04f8751dec698971251759b
jaemin1005 <ghkdwoals1005@gmail.com>
2024-11-15 23:22:36 +0900

    New | api 라우터에 요청하는 fetch 함수 작성
    likeFetch : 좋아요를 추가해주는 로직, POST로 보낸다.
    dislikeFetch: 좋아요를 제거해주는 로직, DELETE로 보낸다.

---
+++ web/utils/fetch/like_fetch.ts
@@ -0,0 +1,34 @@
+import { MapLikes } from "@/interface/content.dto";
+import { ObjectId } from "@/interface/objectId";
+
+export const likeFetch = async (
+  mapId: string,
+  accessToken: string
+): Promise<Array<ObjectId>> => {
+  const res = await fetch(`/api/maps/${mapId}/like`, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+
+  const { objectids } = (await res.json()) as MapLikes;
+
+  return objectids;
+};
+
+export const dislikeFetch = async (
+  mapId: string,
+  accessToken: string
+): Promise<Array<ObjectId>> => {
+  const res = await fetch(`/api/maps/${mapId}/like`, {
+    method: "DELETE",
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+
+  const { objectids } = (await res.json()) as MapLikes;
+
+  return objectids;
+};
