c6505dff2f516f4f8349a02d33e217cc19fbf7d3
jaemin1005 <ghkdwoals1005@gmail.com>
2024-11-15 23:25:12 +0900

    Feat | 좋아요에 관련된 사이드 버튼을 눌렀을 때의 로직을 작성 완료
    버트을 눌렀을 때, 버튼의 상태에 따라 fetch 로직이 달라진다.
    fetchd의 결과가 완료될 떄까지, 버튼은 동작하지 않도록 하였다

--- web/components/side/side_button_component.tsx
+++ web/components/side/side_button_component.tsx
@@ -4,22 +4,26 @@ import AddIcon from "@mui/icons-material/Add";
 import IconButton from "./icon_button";
 import RemoveIcon from "@mui/icons-material/Remove";
 import ControlCameraIcon from "@mui/icons-material/ControlCamera";
-import MapIcon from '@mui/icons-material/Map';
+import MapIcon from "@mui/icons-material/Map";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import L from "leaflet";
 import BookmarkIcon from "@mui/icons-material/Bookmark";
-import RoomIcon from '@mui/icons-material/Room';
+import RoomIcon from "@mui/icons-material/Room";
 import { ToggleButton } from "./toggle_button";
-import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
-import SearchIcon from '@mui/icons-material/Search';
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+import SearchIcon from "@mui/icons-material/Search";
+import { useMap } from "@/context/map_context";
+import { useAuth } from "@/context/auth_context";
+import { useEffect, useState } from "react";
+import { dislikeFetch, likeFetch } from "@/utils/fetch/like_fetch";
 
 interface SideButtonComponentProps {
-  map?: L.Map
+  map?: L.Map;
   toggleMark: [boolean, () => void];
-  clickMarkInfo: () => void,
-  clickMapMe: () => void,
-  clickInitMap: () => void,
-  clickSearch: () => void,
+  clickMarkInfo: () => void;
+  clickMapMe: () => void;
+  clickInitMap: () => void;
+  clickSearch: () => void;
 }
 
 const SideButtonComponent: React.FC<SideButtonComponentProps> = ({
@@ -28,8 +32,23 @@ const SideButtonComponent: React.FC<SideButtonComponentProps> = ({
   clickMarkInfo,
   clickMapMe,
   clickInitMap,
-  clickSearch
+  clickSearch,
 }) => {
+  const { user, accessToken } = useAuth();
+  const { likes, id, setLikes } = useMap();
+
+  const [likeSelect, setLikeSelect] = useState<boolean>(false);
+
+  const [loading, setIsLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (!user) {
+      setLikeSelect(false);
+      return;
+    }
+
+    setLikeSelect(likes.some((like) => like.$oid === user.user_id.$oid));
+  }, [likes, user]);
 
   const zoomInFunc = () => {
     map?.zoomIn();
@@ -46,6 +65,29 @@ const SideButtonComponent: React.FC<SideButtonComponentProps> = ({
     });
   };
 
+  const clickLikeBtn = (e: React.MouseEvent<HTMLElement, MouseEvent>) => {
+    if (!user || !id || !accessToken || loading) {
+      e.preventDefault();
+      return;
+    }
+
+    setIsLoading(true);
+    setLikeSelect((prev) => !prev);
+
+    const fetchAction = likeSelect ? dislikeFetch : likeFetch;
+
+    fetchAction(id.$oid, accessToken)
+      .then((value) => {
+        setLikes(value);
+      })
+      .catch((err) => {
+        console.error(`Failed to ${likeSelect ? "dislike" : "like"}:`, err);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
+
   return (
     <div className="absolute h-full flex flex-row-reverse right-0 pt-5 top-0 z-50">
       <div className="h-full flex flex-col pt-5 items-center px-2 gap-y-2">
@@ -58,20 +100,30 @@ const SideButtonComponent: React.FC<SideButtonComponentProps> = ({
         <IconButton className="mt-5" onClick={moveToCurrent}>
           <ControlCameraIcon className="text-white text-2xl group-hover:text-yellow-300" />
         </IconButton>
-        <IconButton>
+        <ToggleButton
+          selected={likeSelect}
+          onClick={clickLikeBtn}
+          originIconColor="white"
+          changeIconColor="#f9a8d4"
+        >
           <FavoriteIcon className="text-white text-2xl group-hover:text-pink-300" />
-        </IconButton>
-        <ToggleButton selected={toggleMark[0]} onChange={toggleMark[1]} originIconColor="white" changeIconColor="#93c5fd"> 
+        </ToggleButton>
+        <ToggleButton
+          selected={toggleMark[0]}
+          onChange={toggleMark[1]}
+          originIconColor="white"
+          changeIconColor="#93c5fd"
+        >
           <RoomIcon className="text-white text-2xl group-hover:text-blue-300" />
         </ToggleButton>
         <IconButton className="mt-5" onClick={clickSearch}>
-          <SearchIcon className="text-white text-2xl group-hover:text-purple-300"/>
+          <SearchIcon className="text-white text-2xl group-hover:text-purple-300" />
         </IconButton>
         <IconButton onClick={clickMarkInfo}>
           <BookmarkIcon className="text-white text-2xl group-hover:text-green-300" />
         </IconButton>
         <IconButton onClick={clickMapMe}>
-          <MapIcon className="text-white group-hover:text-purple-300"/>
+          <MapIcon className="text-white group-hover:text-purple-300" />
         </IconButton>
         <IconButton className="mt-5" onClick={clickInitMap}>
           <DeleteForeverIcon className="text-white text-2xl group-hover:text-gray-500" />
